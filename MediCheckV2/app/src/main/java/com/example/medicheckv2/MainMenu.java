260e22dd9c063e5f7542376fb08b53c20eab2919
coreylynch00 <coreylynch4@gmail.com>
2021-11-02 14:42:30 +0000

    Added MediCheckV2 application

---
+++ MediCheckV2/app/src/main/java/com/example/medicheckv2/MainMenu.java
@@ -0,0 +1,44 @@
+package com.example.medicheckv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
+
+    //Declare variables
+    Button logout, userDetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        //Initialize variables and set onClickListener
+        logout = (Button) findViewById(R.id.buttonLogout);
+        logout.setOnClickListener(this);
+        userDetails = (Button) findViewById(R.id.buttonViewDetails);
+        userDetails.setOnClickListener(this);
+    }
+
+    //onClick method
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            //If logout button is clicked, log user out
+            case R.id.buttonLogout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainMenu.this, MainActivity.class));
+                break;
+            //If user details button is clicked
+            case R.id.buttonViewDetails:
+                startActivity(new Intent(MainMenu.this, UserDetails.class));
+                break;
+        }
+    }
+}
---
+++ MediCheckV2/app/src/main/java/com/example/medicheckv2/User.java
@@ -0,0 +1,19 @@
+package com.example.medicheckv2;
+
+public class User {
+
+    String name, age, email, gender, diabetes, heart;
+
+    public User(){
+
+    }
+
+    public User(String name, String age, String email, String gender, String diabetes, String heart){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.gender = gender;
+        this.diabetes = diabetes;
+        this.heart = heart;
+    }
+}
